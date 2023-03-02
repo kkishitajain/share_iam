eb19621717cc6d8d5e739952971fdc1c45c36dba
Kkishita jain <44028299+kkishitajain@users.noreply.github.com>
2023-03-02 15:57:20 +0530

    Update base.js

--- base.js
+++ base.js
@@ -47,7 +47,7 @@ function initialize() {
         language_country = languageToCountryCode[lan_code[0]];
     }
     else {
-        language_country = 'fil-ph';
+        language_country = 'cs_cz';
     }
     
     url = "Strings/" + language_country + "/translation.json";
